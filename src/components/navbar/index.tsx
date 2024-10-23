d12eaf6b4268e6029366d24bfdd5bd68bee2e89e
Anas Firly <anasfirly20@gmail.com>
2024-10-23 09:38:37 +0700

    feat: adding logo and change title

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
-import logo from "@/assets/logo.svg";
 
 export const Navbar = (): JSX.Element => {
   return (
     <nav className="px-xl py-sm flex justify-between items-center">
       <Link to="/">
-        <img src={logo} width={40} height={40} alt="logo" />
+        <img src="/logo.svg" width={40} height={40} alt="logo" />
       </Link>
     </nav>
   );
